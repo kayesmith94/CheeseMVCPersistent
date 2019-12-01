7f90980d555a2ea960ef4c69332d016bb7cdee9e
kayesmith94 <50887040+kayesmith94@users.noreply.github.com>
2019-12-01 17:51:12 -0500

    did some updates.

---
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseCategory
+    {
+        public string Name { get; set; }
+        public int ID { get; private set; }
+        private int nextID = 1;
+    }
+}
