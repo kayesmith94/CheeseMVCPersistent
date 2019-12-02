c24d391d0d2c1978ca0239cdbdc514fdfd708e86
kayesmith94 <50887040+kayesmith94@users.noreply.github.com>
2019-12-01 20:31:49 -0500

    Completed the first studio.

--- src/CheeseMVC/Controllers/CategoryController.cs
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -4,32 +4,52 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
 using CheeseMVC.Data;
 
 namespace CheeseMVC.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly CheeseDbContext context;
+        private CheeseDbContext context;
 
         public CategoryController(CheeseDbContext dbContext)
         {
             context = dbContext;
-            /*This creates a private field context of type CheeseDbContext. This object will be the mechanism with which we interact 
-             * with objects stored in the database. 
-             * The MVC framework will do the work of creating an instance of CheeseDbContext and 
-             * passing it into our controller's constructor.
-             * 
+            /* 
              * This code would need to be added to each controller class that you want 
              * to have access to the persistent collections defined within CheeseDbContext. */
-
-             List<CheeseCategory> cheesecatobjs = context.Categories.ToList();// is this where I do this? how does index know about context?
         }
 
         public IActionResult Index()
         {
-            List<CheeseCategory> cheesecatobjs = context.Categories.ToList();//
-            return View(cheesecatobjs);
+            IList<CheeseCategory> categories = context.Categories.ToList();//
+            return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            return View();       
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryVCM)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = addCategoryVCM.Name
+                };
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+            else
+            { 
+                return View(); 
+            }
         }
     }
 }
